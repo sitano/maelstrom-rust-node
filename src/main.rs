abe92112d8129670c6ba380c779c30b0f48c3d25
Ivan Prisyazhnyy <john.koepi@gmail.com>
2023-04-06 15:38:52 +0200

    move to tokio solely

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use async_std::io::stdin;
-use async_std::io::stdout;
-use async_std::io::BufReader;
-use async_std::prelude::*;
-use async_std::task;
-use futures::StreamExt;
 use log::{debug, info};
+use tokio::io::AsyncBufReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::io::{stdin, stdout, BufReader};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
@@ -12,23 +9,20 @@ pub(crate) fn main() -> Result<()> {
     env_logger::init();
     debug!("inited");
 
-    task::block_on(try_main())
+    let runtime = tokio::runtime::Runtime::new()?;
+    let _guard = runtime.enter();
+
+    runtime.block_on(try_main())
 }
 
 async fn try_main() -> Result<()> {
     let stdin = BufReader::new(stdin());
     let mut stdout = stdout();
-    let mut lines_from_stdin = futures::StreamExt::fuse(stdin.lines());
-    loop {
-        match lines_from_stdin.next().await {
-            Some(res) => {
-                let line = res?;
-                info!("Received {}", line);
-                stdout.write_all(line.as_bytes()).await?;
-                stdout.write_all(b"\n").await?;
-            }
-            None => break,
-        }
+    let mut lines_from_stdin = stdin.lines();
+    while let Some(line) = lines_from_stdin.next_line().await? {
+        info!("Received {}", line);
+        stdout.write_all(line.as_bytes()).await?;
+        stdout.write_all(b"\n").await?;
     }
     Ok(())
 }
